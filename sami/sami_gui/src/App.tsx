061dc0efb41b769c0fdf5b519841deface7701b6
edwardgallyot <77062660+edwardgallyot@users.noreply.github.com>
2023-04-02 22:42:49 +0100

    Adding the sami-gui folder in

---
+++ sami/sami_gui/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import protobuf from 'protobufjs'
+
+function App() {
+  const [value, setValue] = useState(50);
+
+  return (
+    <div className="App">
+        <div>
+            <input
+                type={"range"}
+                min={1}
+                max={100}
+                value={value}
+                className={"slider"}
+                onChange={(event)=> {setValue(event.target.value as number)}}
+            />
+        </div>
+    </div>
+  )
+}
+
+export default App
