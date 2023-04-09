63500b91af10e5ad77fa2a5ce0e813f8f9535ecf
Edward Gallyot <edwardgallyot@Edwards-Mac-mini.local>
2023-04-09 14:45:02 +0100

    Removing protobuf for good cus it's awful...

--- sami/GUI/sami_Webview.cpp
+++ sami/GUI/sami_Webview.cpp
@@ -1,4 +1,6 @@
 #include "sami_WebView.h"
+#include "../proto/messages.pb.h"
+#include <memory>
 
 sami::WebView::WebView(bool enableDevTools)
     : choc::ui::WebView({
@@ -14,6 +16,9 @@ sami::WebView::~WebView()
 void sami::WebView::handleWebviewInvocation(const std::string & msg)
 {
     if (juce::MessageManager::getInstanceWithoutCreating()->isThisTheMessageThread()) {
+        auto message = std::make_unique<sami::Test>();
+        message->ParseFromString(msg);
+        std::cout << message->message() << std::endl; 
     } else {
         // We don't know how we will handle this if it's not on another thread at the moment.
         // At least we can assert here and figure out what's going on later...
--- sami/GUI/sami_gui/src/App.tsx
+++ sami/GUI/sami_gui/src/App.tsx
@@ -1,19 +1,40 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import protobuf from 'protobufjs'
+import protobuf, { Root } from 'protobufjs'
+import { TextDecoder } from 'text-decoding'
 
 function App() {
     const [value, setValue] = useState(50);
-    const loadProtobuf = () => { protobuf.load("messages.proto", (err, root) => {
+    const [root, setRoot] = useState(new protobuf.Root);
+
+
+    const loadProtobuf = () => { protobuf.load("./messages.proto", root, (err, root) => {
             if (err) 
                 throw err;
-            console.log(root);
+            setRoot(root as Root);
         });
     };
+
     useEffect(() => {
         loadProtobuf();
         return () => {};
     }, [])
+
+    const handleValueChange = (event: any) => {
+        setValue(event.target.value as any);
+        var test = root.lookupType("sami.Test");
+        var payload = {
+            message: event.target.value 
+        };
+        var err = test.verify(payload);
+        console.log(err);
+        var target = test.create(payload);
+        const buffer = test.encode(target).finish();
+        const stringpayload = new TextDecoder().decode(buffer);
+        console.log(stringpayload);
+        (window as any).external.invoke(stringpayload);
+    };
+
     return (
       <div className="App">
           <div>
@@ -23,7 +44,7 @@ function App() {
                   max={100}
                   value={value}
                   className={"slider"}
-                  onChange={(event)=> {setValue(event.target.value as any)}}
+                  onChange={handleValueChange}
               />
           </div>
       </div>
