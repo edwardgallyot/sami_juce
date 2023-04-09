9028ffea1d6b806ff59c658f597029fdb20d3762
Edward Gallyot <edwardgallyot@Edwards-Mac-mini.local>
2023-04-09 21:33:20 +0100

    removing text decdoing dependencies

--- sami/GUI/sami_gui/src/App.tsx
+++ sami/GUI/sami_gui/src/App.tsx
@@ -1,38 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import protobuf, { Root } from 'protobufjs'
-import { TextDecoder } from 'text-decoding'
 
 function App() {
     const [value, setValue] = useState(50);
     const [root, setRoot] = useState(new protobuf.Root);
 
-
-    const loadProtobuf = () => { protobuf.load("./messages.proto", root, (err, root) => {
-            if (err) 
-                throw err;
-            setRoot(root as Root);
-        });
-    };
-
-    useEffect(() => {
-        loadProtobuf();
-        return () => {};
-    }, [])
-
     const handleValueChange = (event: any) => {
         setValue(event.target.value as any);
-        var test = root.lookupType("sami.Test");
-        var payload = {
-            message: event.target.value 
-        };
-        var err = test.verify(payload);
-        console.log(err);
-        var target = test.create(payload);
-        const buffer = test.encode(target).finish();
-        const stringpayload = new TextDecoder().decode(buffer);
-        console.log(stringpayload);
-        (window as any).external.invoke(stringpayload);
     };
 
     return (
